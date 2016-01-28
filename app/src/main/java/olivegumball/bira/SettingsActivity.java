9a9fa92af225d9aef993b61af32505a5262d607b
Ariel Schon <ariel.schon12@gmail.com>
2016-01-29 00:42:50 +0200

    Fixed white status bar in main activity. Also added new empty settings activity.

---
+++ app/src/main/java/olivegumball/bira/SettingsActivity.java
@@ -0,0 +1,17 @@
+package olivegumball.bira;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ariel on 1/29/16.
+ */
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+    }
+}
