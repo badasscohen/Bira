f9225cd0f3ec75ce56423818fa600d97e3fb44aa
Ariel Schon <ariel.schon12@gmail.com>
2016-01-29 03:15:54 +0200

    Added basic design for settings activity.

--- app/src/main/java/olivegumball/bira/SettingsActivity.java
+++ app/src/main/java/olivegumball/bira/SettingsActivity.java
@@ -1,7 +1,10 @@
 package olivegumball.bira;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.widget.EditText;
 
 /**
  * Created by ariel on 1/29/16.
@@ -12,6 +15,8 @@ public class SettingsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        ((EditText) findViewById(R.id.add_beer)).getBackground().setColorFilter(Color.parseColor("#ffffff"), PorterDuff.Mode.SRC_IN);
 
     }
 }
